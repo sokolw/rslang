ad612e82ee3180dcea628f5415f089e3f96eb58c
sokolw <sokolw21@gmail.com>
2022-08-19 23:57:34 +0300

    feat: add new components for declarations

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import HeaderComponent from './header/header.component';
+import AuthorizationButtonsComponent from './authorization-buttons/authorization-buttons.component';
+import DropdownMenuComponent from './dropdown-menu/dropdown-menu.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
-  imports: [],
+  declarations: [HeaderComponent, AuthorizationButtonsComponent, DropdownMenuComponent],
+  imports: [CommonModule],
   exports: [HeaderComponent],
 })
 export default class SharedModule {}
