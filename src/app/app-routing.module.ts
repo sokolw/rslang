f18b28897b7bc423f9b23be9c617c0257ef56ca6
Pavel Zabalotny <pavelzabalotny@gmail.com>
2022-08-23 22:37:06 +0300

    feat: add main-page-container to the routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import MainPageContainerComponent from './main-page/main-page-container/main-page-container.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: MainPageContainerComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
