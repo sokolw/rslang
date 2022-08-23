e2c15a7ebdde8f88859d41c153515513508e6d68
Pavel Zabalotny <pavelzabalotny@gmail.com>
2022-08-23 22:23:42 +0300

    feat: create main-page-container component

---
+++ src/app/main-page/main-page-container/main-page-container.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-page-container',
+  templateUrl: './main-page-container.component.html',
+  styleUrls: ['./main-page-container.component.scss'],
+})
+export default class MainPageContainerComponent {}
--- src/app/main-page/main-page.module.ts
+++ src/app/main-page/main-page.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import MainPageContainerComponent from './main-page-container/main-page-container.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [MainPageContainerComponent],
   imports: [CommonModule],
 })
 export default class MainPageModule {}
