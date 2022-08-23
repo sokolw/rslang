40112e4ba5d3f4199f025ae29fb4a56a290e4e76
sokolw <sokolw21@gmail.com>
2022-08-23 16:42:30 +0300

    feat: add modules: Forms, MatProgressSpinner. add components: Register, Login, PopupMessage

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import HeaderComponent from './header/header.component';
 import AuthorizationButtonsComponent from './authorization-buttons/authorization-buttons.component';
 import DropdownMenuComponent from './dropdown-menu/dropdown-menu.component';
 import UserAccountComponent from './user-account/user-account.component';
+import RegisterComponent from './register/register.component';
+import PopupMessageComponent from './popup-message/popup-message.component';
+import LoginComponent from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -11,8 +16,12 @@ import UserAccountComponent from './user-account/user-account.component';
     AuthorizationButtonsComponent,
     DropdownMenuComponent,
     UserAccountComponent,
+    RegisterComponent,
+    LoginComponent,
+    PopupMessageComponent,
   ],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, MatProgressSpinnerModule],
+
   exports: [HeaderComponent],
 })
 export default class SharedModule {}
