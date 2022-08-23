afe149bff65d34ebeee0496a9b69b040f1017e43
Pavel Zabalotny <pavelzabalotny@gmail.com>
2022-08-23 22:50:12 +0300

    fix: use default import page-not-found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import MainPageContainerComponent from './main-page/main-page-container/main-page-container.component';
-import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
+import PageNotFoundComponent from './shared/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', component: MainPageContainerComponent },
