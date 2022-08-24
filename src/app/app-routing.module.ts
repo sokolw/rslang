9d29900fbec5760f2416d4f8cf527049dbfff112
Wex2 <gatsqp@gmail.com>
2022-08-24 13:16:10 +0300

    feat: add textbook routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import TextBookPageContainerComponent from './text-book/text-book-page-container/text-book-page-container.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'textbook', component: TextBookPageContainerComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
