e1a9b7b193940870d7184fcce601ecd5ce46da19
Wex2 <gatsqp@gmail.com>
2022-08-24 12:43:07 +0300

    feat: add empty text book word component

---
+++ src/app/text-book/text-book-word/text-book-word.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-book-word',
+  templateUrl: './text-book-word.component.html',
+  styleUrls: ['./text-book-word.component.scss'],
+})
+export default class TextBookWordComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/text-book/text-book.module.ts
+++ src/app/text-book/text-book.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import TextBookPageContainerComponent from './text-book-page-container/text-book-page-container.component';
 import TextBookGroupControlsComponent from './text-book-group-controls/text-book-group-controls.component';
 import TextBookPageControlsComponent from './text-book-page-controls/text-book-page-controls.component';
+import TextBookWordComponent from './text-book-word/text-book-word.component';
 
 @NgModule({
   declarations: [
     TextBookPageContainerComponent,
     TextBookGroupControlsComponent,
     TextBookPageControlsComponent,
+    TextBookWordComponent,
   ],
   imports: [CommonModule],
   exports: [TextBookPageContainerComponent],
