b10b0ee4470ac29ce4aadcba0bc22f073dbffc40
Wex2 <gatsqp@gmail.com>
2022-08-24 12:32:18 +0300

    feat: add empty text book group controls component

---
+++ src/app/text-book/text-book-group-controls/text-book-group-controls.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-book-group-controls',
+  templateUrl: './text-book-group-controls.component.html',
+  styleUrls: ['./text-book-group-controls.component.scss'],
+})
+export default class TextBookGroupControlsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/text-book/text-book.module.ts
+++ src/app/text-book/text-book.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import TextBookPageContainerComponent from './text-book-page-container/text-book-page-container.component';
+import TextBookGroupControlsComponent from './text-book-group-controls/text-book-group-controls.component';
 
 @NgModule({
-  declarations: [TextBookPageContainerComponent],
+  declarations: [TextBookPageContainerComponent, TextBookGroupControlsComponent],
   imports: [CommonModule],
   exports: [TextBookPageContainerComponent],
 })
