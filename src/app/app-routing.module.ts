9f89fb9cff1b6b00fa5d408757ada34fce3e1dd2
sokolw <sokolw21@gmail.com>
2022-09-01 16:30:05 +0300

    feat: add route for game audio call

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GAME_AUDIO_CALL } from './core/constants/constants';
+import GameAudioCallContainerComponent from './game-audio-call/game-audio-call-container/game-audio-call-container.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: GAME_AUDIO_CALL, component: GameAudioCallContainerComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+
   exports: [RouterModule],
 })
 export default class AppRoutingModule {}
