58dd71391e9d27249ca6dcb7afc5b05dcc431104
Wex2 <gatsqp@gmail.com>
2022-09-03 13:21:39 +0300

    feat: implement text book group controls component

--- src/app/text-book/text-book-group-controls/text-book-group-controls.component.ts
+++ src/app/text-book/text-book-group-controls/text-book-group-controls.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Output, Input } from '@angular/core';
 import { MatButtonToggleChange } from '@angular/material/button-toggle';
 
 @Component({
@@ -6,15 +6,14 @@ import { MatButtonToggleChange } from '@angular/material/button-toggle';
   templateUrl: './text-book-group-controls.component.html',
   styleUrls: ['./text-book-group-controls.component.scss'],
 })
-export default class TextBookGroupControlsComponent implements OnInit {
+export default class TextBookGroupControlsComponent {
+  @Input() isAuthorized: boolean = false;
+
   @Output() groupChanged = new EventEmitter<MatButtonToggleChange>();
 
-  groupsStyles = ['#e0e0e0', '#64b5f6', '#81c784', '#fff176', '#de7553', '#e57373', '#e33434'];
+  groupsStyles = ['group1', 'group2', 'group3', 'group4', 'group5', 'group6'];
 
   getPaginatorData(event: MatButtonToggleChange) {
     this.groupChanged.emit(event);
-    console.log(event);
   }
-
-  ngOnInit(): void {}
 }
