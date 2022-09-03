40d2ba9486f3bca54bfaf1c3a394b55ddeab6ac7
Wex2 <gatsqp@gmail.com>
2022-09-03 13:11:44 +0300

    refactor: delete ng bootstrap lib

--- src/app/text-book/text-book.module.ts
+++ src/app/text-book/text-book.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
-import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { RouterModule } from '@angular/router';
 import TextBookPageContainerComponent from './text-book-page-container/text-book-page-container.component';
 import TextBookGroupControlsComponent from './text-book-group-controls/text-book-group-controls.component';
 import TextBookPageControlsComponent from './text-book-page-controls/text-book-page-controls.component';
@@ -15,7 +15,7 @@ import TextBookWordComponent from './text-book-word/text-book-word.component';
     TextBookPageControlsComponent,
     TextBookWordComponent,
   ],
-  imports: [CommonModule, MatPaginatorModule, MatButtonToggleModule, NgbPaginationModule],
+  imports: [CommonModule, MatPaginatorModule, MatButtonToggleModule, RouterModule],
   exports: [TextBookPageContainerComponent],
 })
 export default class TextBookModule {}
