2db844ec4f4dbe436bd19eaebcba834c7429fe75
Pavel Zabalotny <pavelzabalotny@gmail.com>
2022-09-04 01:07:51 +0300

    feat: implement timer component

---
+++ src/app/game-sprint/game-process/timer/timer.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-timer',
+  templateUrl: './timer.component.html',
+  styleUrls: ['./timer.component.scss'],
+})
+export default class TimerComponent implements OnInit, OnDestroy {
+  timeLimit = 60;
+
+  timeLeft = this.timeLimit;
+
+  timerInterval = 0;
+
+  fullDashArray = 280;
+
+  firstDashArray = this.fullDashArray;
+
+  circleDashArray: string = '';
+
+  strokeColor = '#6200ea';
+
+  calculateTimeFraction() {
+    const rawTimeFraction = this.timeLeft / this.timeLimit;
+    return rawTimeFraction - (1 / this.timeLimit) * (1 - rawTimeFraction);
+  }
+
+  setCircleDasharray() {
+    this.firstDashArray = this.calculateTimeFraction() * this.fullDashArray;
+    this.circleDashArray = `${this.firstDashArray.toFixed(0)} ${this.fullDashArray}`;
+  }
+
+  ngOnInit() {
+    this.timerInterval = setInterval(() => {
+      if (this.timeLeft === 0) {
+        clearInterval(this.timerInterval);
+        // TODO: replace console.log to necessary method
+        console.log('time is up');
+        return;
+      }
+      if (this.timeLeft === 11) {
+        this.strokeColor = 'red';
+      }
+      this.timeLeft -= 1;
+      this.setCircleDasharray();
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.timerInterval);
+  }
+}
--- src/app/game-sprint/game-sprint.module.ts
+++ src/app/game-sprint/game-sprint.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import GameSprintContainerComponent from './game-sprint-container/game-sprint-container.component';
 import GameProcessComponent from './game-process/game-process.component';
+import TimerComponent from './game-process/timer/timer.component';
 
 @NgModule({
-  declarations: [GameSprintContainerComponent, GameProcessComponent],
+  declarations: [GameSprintContainerComponent, GameProcessComponent, TimerComponent],
   imports: [CommonModule],
 
   exports: [GameSprintContainerComponent],
