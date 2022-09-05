580c6cffd991f0e64d3415c2396caadc748a29b4
sokolw <sokolw21@gmail.com>
2022-09-05 19:28:06 +0300

    fix: import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import PageNotFoundComponent from './shared/page-not-found/page-not-found.compon
 import { GAME_AUDIO_CALL, GAME_SPRINT, STATS, TEXT_BOOK } from './core/constants/constants';
 import GameAudioCallContainerComponent from './game-audio-call/game-audio-call-container/game-audio-call-container.component';
 import StatisticPageContainerComponent from './statistic-page/statistic-page-container/statistic-page-container.component';
-import GameSprintContainerComponent from './game-sprint/game-sprint-container/game-sprint-container.component';
+import { GameSprintContainerComponent } from './game-sprint/game-sprint-container/game-sprint-container.component';
 
 const routes: Routes = [
   { path: GAME_AUDIO_CALL, component: GameAudioCallContainerComponent },
