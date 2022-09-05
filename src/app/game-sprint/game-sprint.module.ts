69f1aacb04db5617bceb70bebdc55e26d06ffe56
sokolw <sokolw21@gmail.com>
2022-09-05 19:28:35 +0300

    feat: add new GameResultComponent

--- src/app/game-sprint/game-sprint.module.ts
+++ src/app/game-sprint/game-sprint.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import GameSprintContainerComponent from './game-sprint-container/game-sprint-container.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatButtonModule } from '@angular/material/button';
+import { GameSprintContainerComponent } from './game-sprint-container/game-sprint-container.component';
 import GameProcessComponent from './game-process/game-process.component';
 import TimerComponent from './game-process/timer/timer.component';
 import GameSelectDifficultyComponent from './game-select-difficulty/game-select-difficulty.component';
 import GameAudioCallModule from '../game-audio-call/game-audio-call.module';
+import GameResultComponent from './game-result/game-result.component';
 
 @NgModule({
   declarations: [
@@ -12,8 +15,9 @@ import GameAudioCallModule from '../game-audio-call/game-audio-call.module';
     GameProcessComponent,
     TimerComponent,
     GameSelectDifficultyComponent,
+    GameResultComponent,
   ],
-  imports: [CommonModule, GameAudioCallModule],
+  imports: [CommonModule, GameAudioCallModule, MatProgressSpinnerModule, MatButtonModule],
 
   exports: [GameSprintContainerComponent],
 })
